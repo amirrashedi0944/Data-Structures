f9628684628a1405d32862b7c9d0242b62fb1478
amirrashedi0944 <110764458+amirrashedi0944@users.noreply.github.com>
2022-11-30 13:19:34 +0330

    Delete Python/Recursive Functions directory

--- Python/Recursive Functions/001Factorial.py
+++
@@ -1,9 +0,0 @@
-def facturial(number):
-    if number == 0:
-        return 1
-    else:
-        return number * facturial(number - 1)
-
-
-number = int(input("Enter a number: "))
-print(f'{number}! ={facturial(number)}')
